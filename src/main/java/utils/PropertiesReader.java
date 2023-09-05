b48f1267044dfd39c84e05a6d6e14de8778dc183
danielslechta <dan.slechta@gmail.com>
2023-09-05 14:18:47 +0200

    Add properties to pom.xml for jenkins execution, modify properties reader. Remove config.properties

--- src/main/java/utils/PropertiesReader.java
+++ src/main/java/utils/PropertiesReader.java
@@ -1,29 +1,12 @@
 package utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class PropertiesReader {
 
-    private static final String baseDir = System.getProperty("user.dir");
-    private static final Properties properties = new Properties();
-
-    static {
-        try {
-            FileInputStream data = new FileInputStream(baseDir + "/src/main/resources/configs/config.properties");
-            properties.load(data);
-        } catch (IOException e) {
-            //noinspection CallToPrintStackTrace
-            e.printStackTrace();
-        }
-    }
-
     public static String getBrowser() {
-        return properties.getProperty("browser");
+        return System.getProperty("browser", "chrome"); // Provide a default value if not set
     }
 
     public static String getEnvironment() {
-        return properties.getProperty("environment");
+        return System.getProperty("environment", "DEV"); // Provide a default value if not set
     }
 }
